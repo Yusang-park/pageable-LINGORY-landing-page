19df0b3345225856f9e8d739480caaa5cf446e3b
JS <66022264+Jsgithubchannel@users.noreply.github.com>
2021-01-20 13:45:08 +0900

    HTML: remove Lorem ipsum

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ var pages = new Pageable("#hompage", {
   },
 });
 
+setTimeout(function () {
+  window.scrollTo(0, 1);
+}, 1000);
 
 document.getElementById("gif").src =
   "img/logo_good_quality.gif?a=" + Math.random();
